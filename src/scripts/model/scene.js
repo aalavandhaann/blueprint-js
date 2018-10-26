4b0a6fa3883edd11ce7fefc639bb841fc7df96c4
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2018-10-26 15:09:02 -0400

    Fixing gridfloor error

--- src/scripts/model/scene.js
+++ src/scripts/model/scene.js
@@ -20,7 +20,7 @@ export class Scene extends EventDispatcher
 		this.model = model;
 		this.textureDir = textureDir;
 		
-		var grid = new GridHelper(100, 5);
+		var grid = new GridHelper(4000, 5);
 		
 		this.scene = new ThreeScene();
 		this.scene.background = new Color(0xffffff);
