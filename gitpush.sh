434083debc385aa27e2826e743fd5c3605e9be33
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2018-10-26 13:10:21 -0400

    ""

--- gitpush.sh
+++ gitpush.sh
@@ -2,19 +2,22 @@
 echo "\"$1\"";
 #Reference: http://lea.verou.me/2011/10/easily-keep-gh-pages-in-sync-with-master/
 
-$ git add .
-$ git status // to see what changes are going to be commited
-$ git commit -m "\"$1\""
-$ git push origin master
+git add .
 
+#to see what changes are going to be commited
+git status
+git commit -m "\"$1\""
+git push origin master
+
+cd ./build
 #go to the gh-pages branch
-$ git checkout gh-pages 
+git checkout gh-pages 
 
 #bring gh-pages up to date with master
-$ git rebase master
+git rebase master
 
 #commit the changes
-$ git push origin gh-pages
+git push origin gh-pages
 
 #return to the master branch
-$ git checkout master
+git checkout master
