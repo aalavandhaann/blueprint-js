4dc21cbfb773710167840055c5ad20c0a97ceb65
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2018-11-13 14:17:26 -0500

    Fixing the bug in gltf_inventory.py

--- models/gltf_inventory.py
+++ models/gltf_inventory.py
@@ -74,18 +74,22 @@ def createItemsJSON(gltf_files, thumbnails, objfiles=None):
         gltf_type = os.path.basename(os.path.dirname(os.path.realpath(gltf_file_path)));
         
         thumbnail_map = findEquivalent(gltf_name, thumbnails);
-        thumbnail_path = thumbnail_map['filepath'];
-        
-        export_gltf_path = os.path.join(export_dir_gltf, gltf_map['filename']);
-        export_gltf_r_path = os.path.relpath(export_gltf_path, start=export_path_root);
-        
-        export_thumb_path = os.path.join(export_dir_thumbnails, thumbnail_map['filename']);
-        export_thumb_r_path = os.path.relpath(export_thumb_path, start=export_path_root);               
-        
-        copyfile(gltf_file_path, export_gltf_path);
-        copyfile(thumbnail_path, export_thumb_path);
-        
-        json_gltf_items.append((gltf_name, gltf_type, export_gltf_r_path, export_thumb_r_path));
+        try:
+            thumbnail_path = thumbnail_map['filepath'];
+            
+            export_gltf_path = os.path.join(export_dir_gltf, gltf_map['filename']);
+            export_gltf_r_path = os.path.relpath(export_gltf_path, start=export_path_root);
+            
+            export_thumb_path = os.path.join(export_dir_thumbnails, thumbnail_map['filename']);
+            export_thumb_r_path = os.path.relpath(export_thumb_path, start=export_path_root);               
+            
+            copyfile(gltf_file_path, export_gltf_path);
+            copyfile(thumbnail_path, export_thumb_path);
+            
+            json_gltf_items.append((gltf_name, gltf_type, export_gltf_r_path, export_thumb_r_path));
+        except TypeError:
+            print('NO THUMBNAIL FOR %s SO SKIPPING THIS INVENTORY ITEM'%(gltf_name));
+            pass;
         
     
     createJSONJS(json_gltf_items, export_path_gltf_json);
@@ -101,7 +105,7 @@ def resizeImages(imagefilesmap):
     
     for im_map in imagefilesmap:
         im_path = im_map['filepath'];
-        im_purename = im_map['purename'];        
+        im_purename = im_map['purename'];
         in_img = Image.open(im_path);
         w, h = in_img.size;
         rw, rh = w, h;
@@ -118,29 +122,50 @@ def resizeImages(imagefilesmap):
         l, t = int(cx - (w*0.5)), int(cy - (h * 0.5));
         out_img.paste(in_img, (l, t));
         out_img.save(out_path);
+        
+        #~ print(out_path.split('.'));
+        #~ if(len(out_path.split('.')) > 2):
+            #~ print('PURENAME ::: ', im_purename);
+            #~ print('OUTPATH ::: ', out_path);
+            #~ raise IndexError;
     
-    input_thumbnails = os.path.abspath('./thumbnails');
-    thumbail_resized_files =  getFilesFromDirectory(thumbnails_resized, extension='.png');
-    return thumbail_resized_files;
+    thumbail_resized_files =  getFilesFromDirectory(thumbnails_resized, extension=['.png']);
+    return thumbnails_resized, thumbail_resized_files;
     
+# The format keyword in items.js needs to written with the below
 in_format = 'gltf';
+# The actual mesh format to look for in the directory of meshes
 in_format_extension = 'glb';
 
+# Where can be the input meshes of format %s(in_format_extension) can be found?
 input_gltf_meshes = os.path.abspath('./%s'%(in_format));
-input_original_thumbnails = os.path.abspath('./thumbnails');
-input_thumbnails = os.path.abspath('./thumbnails_resized');
+# Where can be the raw thumbnails of varying sizes (not 300 x225) can be found?
+input_original_thumbnails = os.path.abspath('./thumbnails/');
+# The path to the thumbnails (that are resized) to be used for showing the inventory item on the app page
+input_thumbnails = os.path.abspath('./thumbnails_resized/');
 
+# Where should I export the generated files?
 export_path_root = os.path.abspath('../build/');
+# The name of the final items.js file will be items_%s(in_format).json. This ensures multiple formats can be used
+# Right now the framework supports original project's json and GLTF format. More import formats need to be added
+# But this is a huge discussion, should we keep it standard to GLTF or support multiple formats?
 export_path_gltf_json = os.path.abspath('../build/js/items_%s.js'%(in_format));
 
+# Path where all the meshes will be exported inside the build folder. This is where Blueprint-js will load the meshs(furnitures) from
 export_dir_gltf = createDir(os.path.abspath('../build/models/%s/'%(in_format)));
+# Path where all the thumbnails will be exported inside the build folder. This is where the inventory page displays the thumbnails of each furniture
 export_dir_thumbnails = createDir(os.path.abspath('../build/models/thumbnails_new/'));
 
+# Now get all the GLTF files from the input_gltf_meshes location
 gltf_mesh_files = getFilesFromDirectory(input_gltf_meshes, extension=['.%s'%(in_format_extension), '.gltf'], recursive=True);
-thumbail_files =  getFilesFromDirectory(input_thumbnails, extension=['.png']);
+# Sort the dictionary of gltf_file_maps alphabetically
 gltf_mesh_files = sorted(gltf_mesh_files, key=lambda k: k['purename']);
 
+# Now Resizing of images is a compulsory process. This way the user has less worries. 
+#thumbail_files =  getFilesFromDirectory(input_thumbnails, extension=['.png']);
+input_thumbnails, thumbnail_files = resizeImages(getFilesFromDirectory(input_original_thumbnails, extension=['.png']));
+
 if __name__ == "__main__":
-    resizeImages(getFilesFromDirectory(input_original_thumbnails, extension='.png'));
-    createItemsJSON(gltf_mesh_files, thumbail_files);
+    # Create the json files with all the inputs gathered so far and export the necessary files and folders
+    createItemsJSON(gltf_mesh_files, thumbnail_files);
     
