9d2d1651fc9a6596585b49a4cc354d35c4a7d66b
Srinivasan Ramachandran <ashok.srinivasan2002@gmail.com>
2019-06-11 13:39:42 -0400

    The roof item was getting stuck to the floor when added to the plan, i.e default poistion of (0,0,0). So find the closest ceiling from the origin and stick there to be further dragged anywhere on the ceiling by the user

--- src/scripts/items/roof_item.js
+++ src/scripts/items/roof_item.js
@@ -1,5 +1,5 @@
 import {Item} from './item.js';
-import {Matrix4} from 'three';
+import {Matrix4, Triangle, Plane, Vector3} from 'three';
 /**
  * A Floor Item is an entity to be placed related to a floor.
  */
@@ -19,8 +19,8 @@ export class RoofItem extends Item
 		this.canvasPlaneWH.position.set(0, this.getHeight() * -0.5, this.getDepth()*0.5);
 		this.canvasPlaneWD.position.set(0, -this.getHeight(), 0);
 
-		this.remove(this.canvasPlaneWH);
-		this.remove(this.canvasPlaneWD);
+		var co = this.closestCeilingPoint();
+		this.moveToPosition(co);
 	}
 
 	/** Returns an array of planes to use other than the ground plane
@@ -28,5 +28,75 @@ export class RoofItem extends Item
 	customIntersectionPlanes()
 	{
 		return this.model.floorplan.roofPlanes();
+	}
+
+	roofContainsPoint(roof, forpoint)
+	{
+			var g = roof.geometry;
+			var result = {distance: Number.MAX_VALUE, contains: false, point: null, closestPoint: null};
+			var closestPoint = null;
+			for (var i=0;i< g.faces.length;i++)
+			{
+					var f = g.faces[i];
+					var plane = new Plane();
+					var triangle = new Triangle(g.vertices[f.a], g.vertices[f.b], g.vertices[f.c]);
+					var ipoint = new Vector3();
+					var cpoint = new Vector3();
+					var contains = false;
+					var distance = 0.0;
+					closestPoint = triangle.closestPointToPoint(forpoint, cpoint);
+					triangle.getPlane(plane);
+					plane.projectPoint(forpoint, ipoint);
+					contains = triangle.containsPoint(ipoint);
+					distance = plane.distanceToPoint(forpoint);
+					if(distance < result.distance && contains)
+					{
+							result.distance = distance;
+							result.contains = contains;
+							result.point = ipoint;
+							result.closestPoint = closestPoint.clone();
+					}
+			}
+			//No good result so return the closest point of the last triangle in this roof mesh
+			if(result.point == null)
+			{
+					result.closestPoint = closestPoint.clone();
+			}
+
+			return result;
+	}
+
+	closestCeilingPoint()
+	{
+		var roofs = this.model.floorplan.roofPlanes();
+		var roof = null;
+		var globalResult = {distance: Number.MAX_VALUE, point: null};
+		var result = null;
+		for (var i=0;i< roofs.length; i++)
+		{
+				roof = roofs[i];
+				result = this.roofContainsPoint(roof, this.position);
+				if(result.point !=null && result.distance < globalResult.distance && result.contains)
+				{
+						globalResult.distance = result.distance;
+						globalResult.point = result.point.clone();
+				}
+		}
+		//No good results so assign the closestPoint of the last roof in the above iteration
+		if(globalResult.point == null)
+		{
+				return result.closestPoint.clone();
+		}
+		return globalResult.point.clone();
+	}
+
+	/** */
+	placeInRoom()
+	{
+		if (!this.position_set)
+		{
+			var co = this.closestCeilingPoint();
+			this.moveToPosition(co);
+		}
 	}
 }
