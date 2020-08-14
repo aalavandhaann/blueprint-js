a558ce2001d0cafd5c3fe1e7fdd4f78e85066a95
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-14 18:42:43 -0400

    update
    generated blueprint.js  by parcel

--- build/js/blueprint.js
+++ build/js/blueprint.js
@@ -107245,7 +107245,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41227" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34727" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
