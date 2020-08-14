2cc99b2eaffcae319aaf9e0fee0b4aad5c542a5e
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-14 18:24:29 -0400

    updates
    Change/update version number

--- src/scripts/core/version.js
+++ src/scripts/core/version.js
@@ -1,35 +1,28 @@
-export class Version
-{
-	static isVersionHigherThan(version, checkVersion)
-	{
-		if(version != undefined)
-		{
-			checkVersion = checkVersion.replace(/[^\d.-]/g, '').split('.');
-			var givenVersion = version.replace(/[^\d.-]/g, '').split('.');
-			var flag = true;
-			if(checkVersion.length != givenVersion.length)
-			{
-				return false;
-			}
-			for (var i=0;i<checkVersion.length;i++)
-			{
-				var a = parseInt(checkVersion[i]);
-				var b = parseInt(givenVersion[i]);
-				flag &= (a >= b);
-			}
-			return flag;
-			
-		}
-		return false;
-	}
-		
-	static getInformalVersion()
-	{
-		return '0.0.2a';
-	}
-	
-	static getTechnicalVersion()
-	{
-		return '0.0.2a';
-	}
+export class Version {
+    static isVersionHigherThan(version, checkVersion) {
+        if (version != undefined) {
+            checkVersion = checkVersion.replace(/[^\d.-]/g, '').split('.');
+            var givenVersion = version.replace(/[^\d.-]/g, '').split('.');
+            var flag = true;
+            if (checkVersion.length != givenVersion.length) {
+                return false;
+            }
+            for (var i = 0; i < checkVersion.length; i++) {
+                var a = parseInt(checkVersion[i]);
+                var b = parseInt(givenVersion[i]);
+                flag &= (a >= b);
+            }
+            return flag;
+
+        }
+        return false;
+    }
+
+    static getInformalVersion() {
+        return '2.0.1a';
+    }
+
+    static getTechnicalVersion() {
+        return '2.0.1a';
+    }
 }
