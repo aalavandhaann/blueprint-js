a912060d3db24c9be8a170a645f7cdd1980d84e8
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-15 12:55:18 -0400

    Updates
    Fix app/index.html for errors when loading from live(git pages)

--- app/app.c328ef1a.js
+++ app/app.c328ef1a.js
@@ -107297,7 +107297,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35283" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32975" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
