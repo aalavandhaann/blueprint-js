33040b041cf5d82bba6b328d6310e05cfad0ac71
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-15 13:41:10 -0400

    updates path resolve

--- app/main.d2070829.js
+++ app/main.d2070829.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45199" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38351" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=app/main.d2070829.js.map
+//# sourceMappingURL=https://aalavandhaann.github.io/blueprint-js/app/main.d2070829.js.map
--- app/src.e31bb0bc.js
+++ app/src.e31bb0bc.js
@@ -107297,7 +107297,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45199" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38351" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -107474,4 +107474,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=app/src.e31bb0bc.js.map
+//# sourceMappingURL=https://aalavandhaann.github.io/blueprint-js/app/src.e31bb0bc.js.map
