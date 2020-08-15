1ef8d05f61395d32b19a8973ba44df1c8b1711a0
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-15 13:11:37 -0400

    updates
    wip: stupid path issue in application demo index.html fixing

--- app/src.e31bb0bc.js
+++ app/src.e31bb0bc.js
@@ -107297,7 +107297,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38967" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33381" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -107474,4 +107474,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+//# sourceMappingURL=src.e31bb0bc.js.map
