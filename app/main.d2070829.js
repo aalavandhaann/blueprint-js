4a2d1f1b0f6d640e0cfd76794378737198b00e3d
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-15 13:36:53 -0400

    wip: path resolution issue because of parcel

--- app/main.d2070829.js
+++ app/main.d2070829.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37799" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45199" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=main.d2070829.js.map
+//# sourceMappingURL=app/main.d2070829.js.map
