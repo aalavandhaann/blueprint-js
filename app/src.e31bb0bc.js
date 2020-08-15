c43ce92a180b7f2cab08eccabc6111c583e8669d
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-08-15 13:01:32 -0400

    wip: fix not loading in live(github pages)

--- app/src.e31bb0bc.js
+++ app/src.e31bb0bc.js
@@ -107297,7 +107297,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42051" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38967" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
