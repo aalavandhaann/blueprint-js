d2dfc620dfd88d6bdca509f33094b8e2dd50cfa1
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-11-30 14:49:54 -0500

    Updates
    - Public git demo files

--- app/src.e31bb0bc.js
+++ app/src.e31bb0bc.js
@@ -118003,7 +118003,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42575" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39229" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
