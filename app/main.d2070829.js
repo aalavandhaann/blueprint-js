8493ee05fc375dc210ca7aa8c074a3d69b3e3258
aalavandhaann <ashok.srinivasan2002@gmail.com>
2020-12-14 09:45:34 -0500

    Updates
    - Change the design to empty_room.json

--- app/main.d2070829.js
+++ app/main.d2070829.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39335" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35739" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
